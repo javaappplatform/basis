c10d51981cb61d55a73379449b84e107942ca543
Hendrik Renken <h.renken@lodige.com>
2014-04-11 09:49:48 +0200

    Revamped random function to only use simple characters a-zA-Z0-9

--- commonlib/src/github/javaappplatform/commons/util/Strings.java
+++ commonlib/src/github/javaappplatform/commons/util/Strings.java
@@ -8,6 +8,18 @@ public class Strings
 	// table to convert a nibble to a hex char.
 	private static final char[] HEX_CHAR = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
 	private static final FastMersenneTwister FMT = new FastMersenneTwister();
+	private static final char[] CHARACTERS = new char[62];
+	
+	static
+	{
+		int i = 0;
+		for (char c = '0'; c < '9'; c++)
+			CHARACTERS[i++] = c;
+		for (char c = 'a'; c < 'z'; c++)
+			CHARACTERS[i++] = c;
+		for (char c = 'A'; c < 'Z'; c++)
+			CHARACTERS[i++] = c;
+	}
 
 
 	public static String toHexString(byte[] b)
@@ -30,9 +42,9 @@ public class Strings
 	
 	public static final String random(int length)
 	{
-		StringBuilder sb = new StringBuilder();
+		StringBuilder sb = new StringBuilder(length);
 		for (int i = 0; i < length; i++)
-			sb.append(FMT.nextChar());
+			sb.append(CHARACTERS[FMT.nextInt(CHARACTERS.length)]);
 		return sb.toString();
 	}
 
